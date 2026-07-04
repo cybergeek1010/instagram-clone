2044a2c62fea1405e41e1765d0ed5bbd1a956ca7
cybergeek1010 <arshad.ahamed2207@gmail.com>
2026-07-04 16:46:55 +0400

    fixed the bug

--- client/routes/postroutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router= express.Router();
-const {createPost,getPosts}=require("../controller/postcontroller.js")
-router.post("/create-post",createPost)
-router.get("/posts",getPosts)
-module.exports = router;
- 
