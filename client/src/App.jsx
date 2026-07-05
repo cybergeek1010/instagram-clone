7c9d98d3ef5f33e5ac31c3fd584d8d9308804eae
cybergeek1010 <arshad.ahamed2207@gmail.com>
2026-07-05 16:33:12 +0400

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import Sidebar from './Sidebar'
 import Feed from './Feed'
 import Suggestions from './suggestions'
 
+
 function App() {
   return (
     <div className='d-flex vh-100 '>
         <div className='w-20'><Sidebar/></div>
         <div className='w-50 overflow-auto' style={{height:"100vh"}}><Feed/></div>
         <div className='w-30'><Suggestions/></div>
+       
     </div>
   )
 }
--- client/src/Stories.jsx
+++ client/src/Stories.jsx
@@ -1,8 +1,71 @@
 import React from 'react'
-
+const stories = [
+  {
+    id: 1,
+    username: "john_doe",
+    image: "https://i.pravatar.cc/150?img=1",
+  },
+  {
+    id: 2,
+    username: "sarah",
+    image: "https://i.pravatar.cc/150?img=2",
+  },
+  {
+    id: 3,
+    username: "alex",
+    image: "https://i.pravatar.cc/150?img=3",
+  },
+  {
+    id: 4,
+    username: "emma",
+    image: "https://i.pravatar.cc/150?img=4",
+  },
+  {
+    id: 5,
+    username: "david",
+    image: "https://i.pravatar.cc/150?img=6",
+  },
+  {
+    id: 6,
+    username: "jack",
+    image: "https://i.pravatar.cc/150?img=7",
+  },
+  {
+    id: 7,
+    username: "john_doe",
+    image: "https://i.pravatar.cc/150?img=8",
+  },
+  {
+    id: 8,
+    username: "john_doe",
+    image: "https://i.pravatar.cc/150?img=9",
+  },
+  {
+    id: 9,
+    username: "john_doe",
+    image: "https://i.pravatar.cc/150?img=10",
+  },
+];
 function Stories() {
   return (
-    <div className='bg-white rounded shadow-sm mb-4 p-3' style={{height:"100px"}}>stories</div>
+    <div className='card p-3 mb-4'>
+      <div className='d-flex' style={{overflowX:"auto",scrollbarWidth:"5px",gap:"20px",}}>
+        {stories.map((story)=>(
+          <div key={story.id} className='text-center' style={{minwidth:"70px"}}>
+            <img src={story.image} alt="" style={{ width: "65px",
+                height: "65px",
+                borderRadius: "50%",
+                border: "3px solid #ff007f",
+                padding: "2px",
+                objectFit: "cover",}} 
+              />
+              <small>{story.username}</small>
+
+          </div>
+
+        ))}
+      </div>
+    </div>
   )
 }
 
